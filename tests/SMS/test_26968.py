0f245e36c3a1862f4863e93268c1b26d2975e0f7
desktop <desktop@desktop-ubuntu12.4>
2013-07-10 14:54:15 +0200

    26968 done

---
+++ tests/SMS/test_26968.py
@@ -0,0 +1,70 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+class test_main(GaiaTestCase):
+    
+    _link        = "www.wikipedia.o"
+    _TestMsg     = "Test " + _link + " this."
+    
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.messages   = Messages(self)
+        self.browser    = Browser(self)
+        
+        #
+        # Establish which phone number to use.
+        #
+        self.target_telNum = self.UTILS.get_os_variable("GLOBAL_TARGET_SMS_NUM")
+        self.UTILS.logComment("Sending sms to telephone number " + self.target_telNum)
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):
+        self.UTILS.getNetworkConnection()
+        
+        #
+        # Launch messages app.
+        #
+        self.messages.launch()
+          
+        #
+        # Create and send a new test message.
+        #
+        self.messages.createAndSendSMS([self.target_telNum], self._TestMsg)
+          
+        #
+        # Wait for the last message in this thread to be a 'recieved' one
+        # and click the link.
+        #
+        x = self.messages.waitForReceivedMsgInThisThread()
+        self.UTILS.TEST(x, "Received a message.", True)
+        
+        
+
+        boolOK=False
+        try:
+            x.find_element("tag name", "a")
+            boolOK=True
+        except:
+                pass
+        if boolOK:
+        # We found the element.
+            var="elementFound"
+        else:
+        # We didn't find the element.
+            var="elementNotFound"
+        
+        self.UTILS.TEST(var=="elementNotFound", "The web link is in the text message")
