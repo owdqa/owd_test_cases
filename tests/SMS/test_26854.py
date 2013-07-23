dc386d171bc7fb56ab353e3c5f25b3babbc2771e
roy <roy.collings@sogeti.com>
2013-07-23 09:52:41 +0200

    Finalised 26854.

--- tests/SMS/test_26854.py
+++ tests/SMS/test_26854.py
@@ -28,28 +28,39 @@ class test_main(GaiaTestCase):
         self.UTILS.reportResults()
         
     def test_run(self):
+        
+        #
+        # When this is unblocked it will be much quicker to do this
+        # than reset the device.
+        #
+#         self.messages.launch()
+#         self.messages.deleteAllThreads()
 
         #
         # Remember the 'real' current date and time.
         #
-        self.NOW_EPOCH =time.time()
+        self.NOW_EPOCH = time.time()
         _now           = self.UTILS.getDateTimeFromEpochSecs(self.NOW_EPOCH)
 
         #
         #=============================================================================
         #
+        self.UTILS.logResult("info", " ")
+        self.UTILS.logResult("info", "--------------------------")
         self.UTILS.logResult("info", "<b><u>Reading an sms from 2 months ago ...</u></b>")
         _test_month = _now.tm_mon - 2
         x = self.UTILS.setTimeToSpecific(p_month=_test_month)
         expected_str = "%s/%s/%s" % (str(x.tm_mon).zfill(2), str(x.tm_mday).zfill(2), x.tm_year)
           
-        self._sendSMS("2 months ago")         
+        self._sendSMS("2 months ago", True)         
         self._checkTimeStamp(expected_str)
   
  
         #
         #=============================================================================
         #
+        self.UTILS.logResult("info", " ")
+        self.UTILS.logResult("info", "--------------------------")
         self.UTILS.logResult("info", "<b><u>Reading an sms from 7 days ago ...</u></b>")
         _test_day = _now.tm_mday - 7   
         x = self.UTILS.setTimeToSpecific(p_day=_test_day)
@@ -66,19 +77,23 @@ class test_main(GaiaTestCase):
         days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         for i in range(6,1,-1):
   
+            self.UTILS.logResult("info", " ")
+            self.UTILS.logResult("info", "--------------------------")
             _test_day = _now.tm_mday - i     
             x = self.UTILS.setTimeToSpecific(p_day=_test_day)
                 
             _dayname = days[x.tm_wday]
             self.UTILS.logResult("info", "<b><u>Reading an sms from %s days ago (%s) ...</u></b>" % (str(i), _dayname))
               
-            self._sendSMS("DAY: %s" % _dayname)
+            self._sendSMS("DAY: %s (%s days ago)." % (_dayname, str(i)))
             self._checkTimeStamp(_dayname)
               
               
         #
         #=============================================================================
         #
+        self.UTILS.logResult("info", " ")
+        self.UTILS.logResult("info", "--------------------------")
         self.UTILS.logResult("info", "<b><u>Reading an sms from yesterday ...</u></b>")
         _test_day = _now.tm_mday - 1   
         x = self.UTILS.setTimeToSpecific(p_day=_test_day)         
@@ -89,6 +104,8 @@ class test_main(GaiaTestCase):
         #
         #=============================================================================
         #
+        self.UTILS.logResult("info", " ")
+        self.UTILS.logResult("info", "--------------------------")
         self.UTILS.logResult("info", "<b><u>Reading an sms from today ...</u></b>")
         self.data_layer.set_time( self.NOW_EPOCH * 1000)
  
@@ -96,12 +113,20 @@ class test_main(GaiaTestCase):
         self._checkTimeStamp("TODAY")
         
         
-    def _sendSMS(self, p_str):
+    def _sendSMS(self, p_str, p_first_time=False):
         #
-        # Sends an sms.
+        # Sends an sms (a little quicker if not p_firs_time).
         #
         self.messages.launch()
-        self.messages.createAndSendSMS([self.num], p_str)
+        if p_first_time:
+            # No thread yet, so start from scratch.
+            self.messages.createAndSendSMS([self.num], p_str)
+        else:
+            # Thread exists, so just use it.
+            self.messages.openThread(self.num)
+            self.messages.enterSMSMsg(p_str)
+            self.messages.sendSMS()
+            
         self.messages.waitForReceivedMsgInThisThread()
         self.apps.kill_all()
         
@@ -113,15 +138,21 @@ class test_main(GaiaTestCase):
         # This is slow, but I can't find a way to see headers that are off the top
         # of the screen, so I ended up doing it this way.
         #
-        x = self.UTILS.screenShotOnErr()
-        self.UTILS.logResult("info", "(Screenshot and html dump at this point ...)", x)
-        self.UTILS.setTimeToNow()
+        self.UTILS.logResult("info", "Setting device time back to the 'real' date and time.")
+        self.data_layer.set_time(self.NOW_EPOCH * 1000)
+
+        _now = self.UTILS.getDateTimeFromEpochSecs(self.NOW_EPOCH)
+        self.UTILS.waitForDeviceTimeToBe( p_year=_now.tm_year,
+                                          p_month=_now.tm_mon,
+                                          p_day=_now.tm_mday,
+                                          p_hour=_now.tm_hour, 
+                                          p_minute=_now.tm_min)
 
         self.messages.launch()
         self.messages.openThread(self.num)
         x = self.UTILS.getElements(DOM.Messages.message_timestamps, "Message timestamp headers", False)[-1]
         self.UTILS.TEST(p_str.lower() in x.text.lower(), 
-                        "<b>Last message timestamp header contains <u>'%s'</u> (it was '%s').</b>" % \
+                        "<b>Last message timestamp header contains <u>'%s'</u> </b>(it was <b>'%s'</b>)." % \
                         (p_str, x.text))
 
        
