53fab13eb3c86c890e0b1ba0bc26d8c94bcfb6fb
roy <roy.collings@sogeti.com>
2013-07-29 10:53:25 +0200

    Keep browser loaded.

--- tests/CLOCK/test_26348.py
+++ tests/CLOCK/test_26348.py
@@ -37,13 +37,13 @@ class test_main(GaiaTestCase):
         # Launch clock app.
         #
         self.clock.launch()
-        
+         
         #
         # Delete all previous alarms.
         #
         #
         self.clock.deleteAllAlarms() 
-
+ 
         #
         # Create an alarm that is 1 minute in the future.
         #
@@ -53,32 +53,32 @@ class test_main(GaiaTestCase):
         diff_m   = 60 - int(now_mins)
         if diff_m <= 1:
             time.sleep(60)
-        
+         
         now_secs = time.strftime("%S", time.gmtime())
         diff_s   = 60 - int(now_secs)
         if diff_s <= 15:
             time.sleep(diff_s)
-
-
+ 
+ 
         t = datetime.datetime.now() + datetime.timedelta(minutes=1)
-        
+         
         _hour   = t.hour
         _minute = t.minute
         _title  = "Test alarm"
-
+ 
         self.clock.createAlarm(_hour, _minute, _title)
-        
+         
         #
         # Return to the main screen (since this is where the user will
         # most likely be when the alarm goes off).
         #
         self.UTILS.goHome()
-        
+         
         #
         # Check the statusbar icon exists.
         #
         self.UTILS.TEST(self.clock.checkStatusbarIcon(), "Alarm icon is present in statusbar.")
-
+ 
         #
         # Wait for the alarm to start.
         #
