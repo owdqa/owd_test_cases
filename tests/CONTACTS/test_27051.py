9f07cd11eb9a5631735955a041654c29fd9f92fd
roy <roy.collings@sogeti.com>
2013-08-13 12:08:39 +0200

    Added 27051.

---
+++ tests/CONTACTS/test_27051.py
@@ -0,0 +1,60 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+from tests._mock_data.contacts import MockContacts
+import time
+
+class test_main(GaiaTestCase):
+    
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.contacts   = Contacts(self)
+        self.settings   = Settings(self)
+
+        self.hotmail_u = self.UTILS.get_os_variable("HOTMAIL_1_EMAIL")
+        self.hotmail_p = self.UTILS.get_os_variable("HOTMAIL_1_PASS")
+
+        #
+        # Get details of our test contacts.
+        #
+        self.cont = MockContacts().Contact_1
+        self.data_layer.insert_contact(self.cont)
+        
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):
+        #
+        # Set up to use data connection.
+        #
+        self.UTILS.getNetworkConnection()
+        
+        self.contacts.launch()
+
+        x = self.contacts.import_HotmailLogin(self.hotmail_u, self.hotmail_p)
+        if not x or x == "ALLIMPORTED":
+            self.UTILS.logResult(False, "Cannot continue past this point without importing the contacts.")
+            return
+
+        x = self.UTILS.getElements(DOM.Contacts.import_conts_list, "Contact list")
+        cont_count = len(x)
+        x = self.UTILS.getElement(DOM.Contacts.import_num_of_conts, "Number of contacts")
+        self.UTILS.logResult("info", "Detected message '%s'." % x.text)
+        self.UTILS.TEST(str(cont_count) in x.text, "'%s' contains the real count, which is %s." % (x.text, cont_count))
+        
+        
+        
+        
