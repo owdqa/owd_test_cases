73f32ec3544bcf27c6b28ab3d513aa20517be011
roy <roy.collings@sogeti.com>
2013-08-14 13:48:25 +0200

    Added 26889

---
+++ tests/CONTACTS/test_26889.py
@@ -0,0 +1,52 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+
+from gaiatest import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+from tests._mock_data.contacts import MockContacts
+
+class test_main(GaiaTestCase):
+ 
+    def setUp(self):
+            
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.contacts   = Contacts(self)
+        
+        #
+        # Get details of our test contact.
+        #
+        self.cont = MockContacts().Contact_1
+        
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+
+    def test_run(self):
+        
+        #
+        # Launch contacts app.
+        #
+        self.contacts.launch()
+        
+        #
+        # Create our contact.
+        #
+        self.contacts.createNewContact(self.cont)
+        
+        #
+        # Verify our contact details.
+        #
+        self.contacts.checkViewContactDetails(self.cont)
+        
+        
