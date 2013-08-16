07ba9b5d039731a722a3b10761dbcee4101a7925
roy <roy.collings@sogeti.com>
2013-08-16 11:34:51 +0200

    Added 26894

---
+++ tests/CONTACTS/test_26894.py
@@ -0,0 +1,67 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+import time
+
+#
+# Imports particular to this test case.
+#
+from tests._mock_data.contacts import MockContacts
+
+class test_main(GaiaTestCase):
+    
+
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.contacts   = Contacts(self)
+        
+        #
+        # Prepare the contact.
+        #
+        self.cont = MockContacts().Contact_1
+
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):
+        
+        #
+        # Launch contacts app.
+        #
+        self.contacts.launch()
+        self.contacts.startCreateNewContact()
+        
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Screenshot at this point", x)
+
+        x = self.UTILS.getElement(DOM.Contacts.done_button, "Done button")
+        self.UTILS.TEST(not x.is_enabled(), "Done button is not enabled")
+        
+        contFields = self.contacts.getContactFields()
+        
+        #
+        # Put the contact details into each of the fields (this method
+        # clears each field first).
+        #
+        self.contacts.replaceStr(contFields['tel'  ] , self.cont["tel"]["value"])
+        
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Screenshot at this point", x)
+
+        x = self.UTILS.getElement(DOM.Contacts.done_button, "Done button")
+        self.UTILS.TEST(x.is_enabled(), "Done button is not enabled")
+        x.tap()
+        
+        self.contacts.viewContact(self.cont["name"])
+        
+
+        
+        
