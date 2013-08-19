1174de0ba13e3fbafdace32db615c4df4a6e9ae6
desktop <desktop@desktop-ubuntu12.4>
2013-08-19 15:32:40 +0200

    26402 updated

--- tests/CONTACTS/test_26402.py
+++ tests/CONTACTS/test_26402.py
@@ -64,6 +64,6 @@ class test_main(GaiaTestCase):
         #
         # Tap Ok button for confirmation.
         #
-        x = self.UTILS.getElement( ("id", "modal-dialog-confirm-ok"), "OK button", True, 5, False)
+        x = self.UTILS.getElement(DOM.GLOBAL.modal_ok_button1, "OK button", True, 5, False)
         x.tap()
         
