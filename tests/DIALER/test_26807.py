08ed938c2f29602420aabbd2999e24bf2ad01699
roy <roy.collings@sogeti.com>
2013-08-23 13:48:18 +0200

    Added 26807

---
+++ tests/DIALER/test_26807.py
@@ -0,0 +1,41 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+
+class test_main(GaiaTestCase):
+    
+    def setUp(self):
+        # Set up child objects...
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.dialer     = Dialer(self)
+        self.contacts   = Contacts(self)
+        
+        self.num  = self.UTILS.get_os_variable("GLOBAL_TARGET_SMS_NUM")
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):
+        #
+        # Enter a number in the dialer.
+        #
+        self.dialer.launch()
+        self.dialer.enterNumber(self.num)
+        
+        self.dialer.callThisNumber()
+        
+        self.UTILS.switchToFrame(*DOM.Dialer.frame_locator_calling)
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Screenshot of call being made:", x)
+        
+        self.dialer.hangUp()
+        
