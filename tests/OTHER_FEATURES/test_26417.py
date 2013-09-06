d257411eb32c4573e39a3bbc93a7cc74aa427c24
roy <roy.collings@sogeti.com>
2013-09-06 13:50:49 +0200

    Added 26417

---
+++ tests/OTHER_FEATURES/test_26417.py
@@ -0,0 +1,53 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+from tests._mock_data.contacts import MockContacts
+
+class test_main(GaiaTestCase):
+    
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        self.UTILS      = UTILS(self)
+        self.contacts   = Contacts(self)
+        self.messages   = Messages(self)
+
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+
+    def test_run(self):
+    
+        self.contacts.launch()
+        self.messages.launch()
+        self.UTILS.touchHomeButton()
+        time.sleep(1)
+        
+        self.UTILS.touchHomeButton()
+        
+        self.UTILS.switchToFrame(*DOM.Home.frame_locator)
+        
+        self.UTILS.waitForNotElements( ("xpath", 
+                                        "//iframe[contains(@%s,'%s')]" % \
+                                        (DOM.Contacts.frame_locator[0],DOM.Contacts.frame_locator[1])),
+                                      "Contacts iframe", True, 1)
+        
+        self.UTILS.waitForNotElements( ("xpath", 
+                                        "//iframe[contains(@%s,'%s')]" % \
+                                        (DOM.Messages.frame_locator[0],DOM.Messages.frame_locator[1])),
+                                      "Messages iframe", True, 1)
+        
+        self.UTILS.waitForElements( ("xpath", "//div[@class='dockWrapper']"), "Application dock (homescreen)")
+        
+        x = self.UTILS.screenShotOnErr()
+        self.UTILS.logResult("info", "Screenshot:", x)
