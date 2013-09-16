e4bf71ea44ad30371318fa19d156dd6a93788d68
roy <roy.collings@sogeti.com>
2013-09-16 16:50:56 +0200

    Added 26782

---
+++ tests/HOMESCREEN/test_26782.py
@@ -0,0 +1,45 @@
+#
+# Imports which are standard for all test cases.
+#
+import sys
+sys.path.insert(1, "./")
+from gaiatest   import GaiaTestCase
+from OWDTestToolkit import *
+
+#
+# Imports particular to this test case.
+#
+
+class test_main(GaiaTestCase):
+    
+    def setUp(self):
+        #
+        # Set up child objects...
+        #
+        GaiaTestCase.setUp(self)
+        
+        self.UTILS      = UTILS(self)
+        self.Settings   = Settings(self)
+        self.EME        = EverythingMe(self)
+        self.actions    = Actions(self.marionette)
+
+        self.UTILS.setPermission('Homescreen', 'geolocation', 'deny')
+        
+    def tearDown(self):
+        self.UTILS.reportResults()
+        
+    def test_run(self):    
+        #
+        # Make sure 'things' are as we expect them to be first.
+        #
+        self.UTILS.getNetworkConnection()
+
+        #
+        # Launch the 'everything.me' app.
+        #
+        self.UTILS.logResult("info", "Launching EME ...")
+        self.EME.launch()
+        
+        self.EME.removeGroups()
+
+
