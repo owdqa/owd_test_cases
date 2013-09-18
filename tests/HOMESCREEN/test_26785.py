07a463c7f19e982d0a1ea3564de8f51892098e31
roy <roy.collings@sogeti.com>
2013-09-18 18:14:51 +0200

    Moved most of 26785 to an EME method.

--- tests/HOMESCREEN/test_26785.py
+++ tests/HOMESCREEN/test_26785.py
@@ -12,7 +12,7 @@ from OWDTestToolkit import *
 
 class test_main(GaiaTestCase):
     
-    _RESTART_DEVICE = True
+#     _RESTART_DEVICE = True
     
     def setUp(self):
         #
@@ -37,7 +37,6 @@ class test_main(GaiaTestCase):
         # Make sure 'things' are as we expect them to be first.
         #
         self.UTILS.getNetworkConnection()
-        
         self.UTILS.uninstallApp(_appName)
 
         #
@@ -45,64 +44,5 @@ class test_main(GaiaTestCase):
         #
         self.UTILS.logResult("info", "Launching EME ...")
         self.EME.launch()
-        x = self.EME.searchForApp(_appName)
-        x.tap()
+        self.EME.bookmarkApp(_appName)
 
-        self.marionette.switch_to_frame()
-        
-        self.UTILS.waitForElements(DOM.EME.launched_button_bar, "Button bar", False)
-        
-        #
-        # Wait for the bookmarcj option to be enabled (can take a few seconds).
-        #
-        boolOK = False
-        for i in range(0,10):
-            x = self.marionette.find_element(*DOM.EME.launched_button_bookmark)
-            if not x.get_attribute("data-disabled"):
-                boolOK = True
-                break
-
-            time.sleep(6)
-        
-        x = self.UTILS.getElement(DOM.EME.launched_display_button_bar, "Button bar 'displayer' element")
-        x.tap()
-        
-        self.UTILS.TEST(boolOK, "Bookmark button is enabled in the button bar.", True)
-        
-        time.sleep(1)
-        
-        x = self.UTILS.getElement(DOM.EME.launched_button_bookmark, "Button bar - bookmark button")
-        self.UTILS.TEST(not x.get_attribute("data-disabled"), "Bookmark button is enabled.", True)
-        x.tap()
-        
-        self.marionette.switch_to_frame()
-        _boolOK = False
-        x = self.UTILS.getElements(DOM.EME.launched_add_to_homescreen, "Apps to be added to homescreen")
-        for i in x:
-            if i.text == _appName:
-                i.tap()
-                _boolOK = True
-                break
-            
-        self.UTILS.TEST(_boolOK, "Adding '%s' to homescreen (app selected)." % _appName)
-        
-        self.UTILS.switchToFrame(*DOM.EME.add_to_home_screen_frame)
-        x = self.UTILS.screenShotOnErr()
-        self.UTILS.logResult("info", "Screenshot at this point:", x)
-        
-        x = self.UTILS.getElement(DOM.EME.add_to_home_screen_btn, "Add to homescreen (button)")
-        x.tap()
-        
-        self.marionette.switch_to_frame()
-        x = self.UTILS.getElement(DOM.EME.launched_display_button_bar, "Button bar 'displayer' element")
-        x.tap()
-        
-        x = self.UTILS.getElement(DOM.EME.launched_button_bookmark , "Button bar - bookmark button")
-        self.UTILS.TEST(x.get_attribute("data-disabled") == "true", "Bookmark button is now disabled.")
-        
-        self.UTILS.TEST(self.UTILS.findAppIcon(_appName), "'%s' is now installed." % _appName)
-        
-        x = self.UTILS.screenShotOnErr()
-        self.UTILS.logResult("info", "Screenshot of the button bar:", x)
-        
-        
