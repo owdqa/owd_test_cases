d90b4996258a9d3adff1041c241fca43697320e0
villenavg <a.villena7@gmail.com>
2014-03-06 09:50:32 +0100

    Test unblocked. Code re-styled

--- tests/SMS/test_28100.py
+++ tests/SMS/test_28100.py
@@ -4,26 +4,28 @@
 import sys
 sys.path.insert(1, "./")
 from gaiatest   import GaiaTestCase
-from OWDTestToolkit import *
 
 #
 # Imports particular to this test case.
 #
+from OWDTestToolkit import DOM
+from OWDTestToolkit.utils import UTILS
+from OWDTestToolkit.apps import Messages
+from OWDTestToolkit.apps import Dialer
+import time
 
 class test_main(GaiaTestCase):
     
-    _TestMsg     = "Test message."
-    
-    _RESTART_DEVICE = True
+    test_msg     = "Test message."
     
     def setUp(self):
         #
         # Set up child objects...
         #
         GaiaTestCase.setUp(self)
-        self.UTILS      = UTILS(self)
-        self.messages   = Messages(self)
-        self.Dialer      = Dialer(self)
+        self.UTILS = UTILS(self)
+        self.messages = Messages(self)
+        self.Dialer = Dialer(self)
 
         self.num1 = self.UTILS.get_os_variable("GLOBAL_TARGET_SMS_NUM")
         
@@ -43,13 +45,17 @@ class test_main(GaiaTestCase):
         nums = ["123", "1234"]        
         self._testAll(nums, 0)
 
+        self.UTILS.logResult("info", "<b>Check MIXED numbers are ok ...</b>")
+        nums = ["123", "12345678","1234"]        
+        self._testAll(nums, 1)
+
     def _testAll(self, nums, tappable_count):
         sms_nums = ""
         
         for i in range(0,len(nums)):
-            sms_nums = "%s, test%s %s" % (sms_nums, str(i), nums[i])
+            sms_nums = "{}, test{} {}".format(sms_nums, str(i), nums[i])
             
-        sms_msg = "Test numbers: %s." % sms_nums
+        sms_msg = "Test numbers: {}.".format(sms_nums)
         
         #
         # Start from clean for each test run.
@@ -65,34 +71,38 @@ class test_main(GaiaTestCase):
         #
         msg_nums = x.find_elements("tag name", "a")
         
-        self.UTILS.TEST(len(msg_nums) == tappable_count,
-                        "There are <b>%s</b> numbers highlighted in the received text (there were <b>%s</b>)." % \
-                        (tappable_count, len(msg_nums)))
+        description = "There are <b>{}</b> numbers highlighted in the received text (there were <b>{}</b>)." 
+        self.UTILS.TEST(len(msg_nums) == tappable_count, 
+            description.format(tappable_count, len(msg_nums)))
         
-        for i in range(0,len(msg_nums)):
+        for i in range(0, len(msg_nums)):
             msg_nums[i].tap()
+
+            #
+            # Press Call button from options overlay
+            #
+            x = self.UTILS.getElement(DOM.Messages.header_call_btn, "Call button")
+            x.tap()
             
             self.UTILS.switchToFrame(*DOM.Dialer.frame_locator)
             
             #
             # Dialler is started with the number already filled in.
             #
+            description = "The phone number contains '{}' (it was '{}')."
             x = self.UTILS.getElement(DOM.Dialer.phone_number, "Phone number")
             self.UTILS.TEST(nums[i] in x.get_attribute("value"), 
-                            "The phone number contains '%s' (it was '%s')." % (nums[i], x.get_attribute("value")))
+                            description.format(nums[i], x.get_attribute("value")))
             
             #
             # Kill everything, then re-launch the messaging app etc ...
             #
-            self.UTILS.switchToApp("Messages")
-            
-            # In case we're not in the thread already.
-            try:
-                self.wait_for_element_present(*DOM.Messages.threads_list, timeout=1)
-                self.messages.openThread(self.num1)
-            except:
-                pass
+            self.messages.launch()
                 
+            #
+            # This may seem repetitive, but it looks like the referece to the 
+            # a HTML elements is lost when switching from apps
+            #    
             x = self.messages.waitForReceivedMsgInThisThread()
             msg_nums = x.find_elements("tag name", "a")
 
