9e82c2e8516599ad58458f6aa717cb7777743ed2
Francisco J. Cano Sandoval <milacao@gmail.com>
2014-08-29 14:47:40 +0200

    Comments from Pull request

--- tests/MMS/test_29903.py
+++ tests/MMS/test_29903.py
@@ -40,29 +40,22 @@ class test_main(GaiaTestCase):
         #
         self.phone_number = self.UTILS.general.get_os_variable("GLOBAL_TARGET_SMS_NUM")
         self.UTILS.reporting.logComment("Sending mms to telephone number " + self.phone_number)
+        self.data_layer.delete_all_sms()
+        self.UTILS.statusbar.clearAllStatusBarNotifs()
 
     def tearDown(self):
         self.UTILS.reporting.reportResults()
         GaiaTestCase.tearDown(self)
 
     def test_run(self):
-            #
-        # Launch messages app.
-        # Make sure we have no threads (currently blocked - use _RESTART_DEVICE instead).
-        #
         self.messages.launch()
 
         #
-        # Create and Send an MMS
+        # Create and Send all MMS
         #
-        self.messages.createAndSendMMS("image", [self.phone_number], "Test 1")
-        self.messages.closeThread()
-
-        self.messages.createAndSendMMS("image", [self.phone_number], "Test 2")
-        self.messages.closeThread()
-
-        self.messages.createAndSendMMS("image", [self.phone_number], "Test 3")
-        self.messages.closeThread()
+        for msg in ["Test {}".format(i) for i in range(3)]:
+            self.messages.createAndSendMMS("image", [self.phone_number], msg)
+            self.messages.closeThread()
 
         #
         # Enter the thread.
@@ -72,13 +65,12 @@ class test_main(GaiaTestCase):
         #
         # Find the first message.
         #
-        x = self.UTILS.element.getElements(DOM.Messages.message_list, "Message list", False)
+        msg_list = self.UTILS.element.getElements(DOM.Messages.message_list, "Message list", False)
         pos = 0
-        for i in x:
+        for (pos, msg) in enumerate(msg_list):
             # Do the check of each message.
             #
-            self.UTILS.test.TEST(i.find_element("xpath", ".//p/span").text == "Test {}".format(pos + 1),
-                        "The message at position {} contains the string 'Test {}'.".format(pos, pos + 1))
-            self.UTILS.test.TEST("outgoing" in i.get_attribute("class"),
-                        "The message at position " + str(pos) + " is  outgoing.")
-            pos += 1
+            self.UTILS.test.TEST(msg.find_element("css selector", "p span").text == "Test {}".format(pos),
+                        "The message at position {} contains the string 'Test {}'.".format(pos, pos))
+            self.UTILS.test.TEST("outgoing" in msg.get_attribute("class"),
+                        "The message at position {} is  outgoing.".format(pos))
