563b1037b1c249e8ad85fc2e3799863356a15761
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-09-10 11:03:55 +0200

    test updated

--- tests/LOOP/test_36668.py
+++ tests/LOOP/test_36668.py
@@ -1,20 +1,40 @@
+import time
 from gaiatest import GaiaTestCase
 from OWDTestToolkit.utils.utils import UTILS
 from OWDTestToolkit.apps.loop import Loop
+from OWDTestToolkit.apps.settings import Settings
+from OWDTestToolkit import DOM
+
 
 class main(GaiaTestCase):
-
+    
     def setUp(self):
         GaiaTestCase.setUp(self)
         self.UTILS = UTILS(self)
         self.loop = Loop(self)
+        self.settings = Settings(self)
 
+        self.fxa_user = self.UTILS.general.get_os_variable("GLOBAL_FXA_USER")
+        self.fxa_pass = self.UTILS.general.get_os_variable("GLOBAL_FXA_PASS")
         self.connect_to_network()
-        
+
+        self.settings.launch()
+        self.settings.fxa()
+        self.settings.fxa_log_out()
+        self.apps.kill_all()
+        time.sleep(2)
+
     def tearDown(self):
         self.UTILS.reporting.reportResults()
         GaiaTestCase.tearDown(self)
 
     def test_run(self):
         self.loop.launch()
-        self.loop.wizard_or_login(ffox_login=True)
+        result = self.loop.wizard_or_login()
+
+        if result:
+            self.loop.firefox_login(self.fxa_user, self.fxa_pass)
+            self.loop.allow_permission()
+
+            header = ('xpath', DOM.GLOBAL.app_head_specific.format("Firefox Hello"))
+            self.UTILS.element.waitForElements(header, "Loop main view")
