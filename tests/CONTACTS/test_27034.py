142bb7e3556aa984eaed3bf1733b7c11b49d9cad
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-10-06 12:18:18 +0200

    Fixed

--- tests/CONTACTS/test_27034.py
+++ tests/CONTACTS/test_27034.py
@@ -11,11 +11,10 @@
 # There should be an option on Contact settings to import contacts
 # from gmail. It is presented as well as its icon
 #===============================================================================
-
+import time
 import sys
 sys.path.insert(1, "./")
 from gaiatest import GaiaTestCase
-
 from OWDTestToolkit import DOM
 from OWDTestToolkit.utils.utils import UTILS
 from OWDTestToolkit.apps.contacts import Contacts
@@ -26,44 +25,26 @@ from tests._mock_data.contacts import MockContact
 class test_main(GaiaTestCase):
 
     def setUp(self):
-        #
-        # Set up child objects...
-        #
         GaiaTestCase.setUp(self)
         self.UTILS = UTILS(self)
         self.contacts = Contacts(self)
         self.settings = Settings(self)
 
-        #
-        # Get details of our test contacts.
-        #
         self.contact = MockContact()
         self.UTILS.general.insertContact(self.contact)
+        self.connect_to_network()
 
     def tearDown(self):
         self.UTILS.reporting.reportResults()
         GaiaTestCase.tearDown(self)
 
     def test_run(self):
-        #
-        # Set up to use data connection.
-        #
-        self.connect_to_network()
-
-        #
-        # Launch contacts app.
-        #
         self.contacts.launch()
         x = self.UTILS.element.getElement(DOM.Contacts.settings_button, "Settings button")
         x.tap()
 
         x = self.UTILS.element.getElement(DOM.Contacts.import_contacts, "Import button")
+        time.sleep(1)
         x.tap()
 
-        #
-        # Wait for the Gmail button.
-        #
         self.UTILS.element.waitForElements(DOM.Contacts.gmail_button, "Gmail button")
-
-        x = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot and details", x)
--- tests/CONTACTS/test_27038.py
+++ tests/CONTACTS/test_27038.py
@@ -1,13 +1,6 @@
-#
-# Imports which are standard for all test cases.
-#
 import sys
 sys.path.insert(1, "./")
 from gaiatest import GaiaTestCase
-
-#
-# Imports particular to this test case.
-#
 from OWDTestToolkit import DOM
 from OWDTestToolkit.utils.utils import UTILS
 from OWDTestToolkit.apps.contacts import Contacts
@@ -18,11 +11,9 @@ from tests._mock_data.contacts import MockContact
 class test_main(GaiaTestCase):
 
     _RESTART_DEVICE = True
+    _gmail_pseudo_locator = ("data-url", "google")
 
     def setUp(self):
-        #
-        # Set up child objects...
-        #
         GaiaTestCase.setUp(self)
         self.UTILS = UTILS(self)
         self.contacts = Contacts(self)
@@ -31,49 +22,27 @@ class test_main(GaiaTestCase):
         self.gmail_user = self.UTILS.general.get_os_variable("GMAIL_1_USER")
         self.gmail_passwd = self.UTILS.general.get_os_variable("GMAIL_1_PASS")
 
-        #
-        # Create test contacts.
-        #
         self.contact = MockContact()
         self.UTILS.general.insertContact(self.contact)
+        self.connect_to_network()
 
     def tearDown(self):
         self.UTILS.reporting.reportResults()
         GaiaTestCase.tearDown(self)
 
     def test_run(self):
-        #
-        # Set up to use data connection.
-        #
-        self.connect_to_network()
-
-        #
-        # Launch contacts app.
-        #
         self.contacts.launch()
         self.contacts.import_gmail_login(self.gmail_user, self.gmail_passwd, False)
 
-        #
-        # Cancel the login.
-        #
+        # Cancel the login
         self.marionette.switch_to_frame()
-        #
-        # It could be more than one cancel button in this frame
-        #
         cancel = self.UTILS.element.getElements(DOM.Contacts.import_cancel_login, "Cancel icon")
         cancel[-1].tap()
 
-        #
-        # Verify that the gmail frame is closed.
-        #
         self.marionette.switch_to_frame()
         self.UTILS.element.waitForNotElements(("xpath", "//iframe[contains(@{}, '{}')]".\
-                                       format(DOM.Contacts.gmail_frame[0], DOM.Contacts.gmail_frame[1])),
-                                      "Gmail login frame")
+                                    format(self._gmail_pseudo_locator[0], self._gmail_pseudo_locator[1])),
+                                   "Gmail login iframe")
 
         self.UTILS.iframe.switchToFrame(*DOM.Contacts.frame_locator)
-
         self.UTILS.element.waitForElements(DOM.Contacts.import_contacts_header, "Import contacts header")
-
-        x = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot and details", x)
