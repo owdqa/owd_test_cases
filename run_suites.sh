dbec352a60ea4586b5f8f275b1885690a3a6559e
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-10-14 16:37:40 +0200

    New script to run suites separatelly

---
+++ run_suites.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DATE=`date +"%m%d%Y"`
+export RESULTS_DIR="$HOME/Escritorio/RESULTS/SCRIPT/PUBLIC/testrun_$DATE"
+
+mkdir -p $RESULTS_DIR
+
+for suite in "$@"
+do
+    ./run_tests.sh $suite
+    cp -r /tmp/tests/test_123 $RESULTS_DIR/$suite
+done
