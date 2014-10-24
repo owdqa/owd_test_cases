5739c0a0dc51c304001a1222f23cd5616be4cda4
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-10-24 10:58:36 +0200

    New tests for LOOP.

---
+++ tests/LOOP/test_34951.py
@@ -0,0 +1,61 @@
+# OWD-34951:Verify when the user is previously signed in de device with
+# Firefox Account, the registered email will be used as ID
+
+import time
+from gaiatest import GaiaTestCase
+from OWDTestToolkit.utils.utils import UTILS
+from OWDTestToolkit.apps.loop import Loop
+from OWDTestToolkit.apps.settings import Settings
+from OWDTestToolkit import DOM
+
+
+class main(GaiaTestCase):
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+        self.UTILS = UTILS(self)
+        self.loop = Loop(self)
+        self.settings = Settings(self)
+
+        self.fxa_user = self.UTILS.general.get_os_variable("GLOBAL_FXA_USER")
+        self.fxa_pass = self.UTILS.general.get_os_variable("GLOBAL_FXA_PASS")
+
+        self.connect_to_network()
+
+        # Clean start
+        if not self.loop.is_installed():
+            self.loop.install()
+        else:
+            self.loop.launch()
+            try:
+                self.loop.open_settings()
+                self.loop.logout()
+            except:
+                self.UTILS.reporting.logResult('info', "Already logged out")
+
+        self.settings.launch()
+        self.settings.fxa()
+
+        if not self.settings.is_fxa_logged_in():
+            self.settings.fxa_log_in(self.fxa_user, self.fxa_pass)
+
+        self.apps.kill_all()
+        time.sleep(2)
+
+    def tearDown(self):
+        self.UTILS.reporting.reportResults()
+        GaiaTestCase.tearDown(self)
+
+    def test_run(self):
+        # First, login
+        self.loop.launch()
+        result = self.loop.wizard_or_login()
+
+        if result:
+            self.loop.tap_on_firefox_login_button()
+
+            self.UTILS.element.waitForElements(DOM.Loop.app_header, "Loop main view")
+            self.loop.open_settings()
+            login_info_elem = self.UTILS.element.getElement(DOM.Loop.settings_logged_as, "Login info")
+            login_info = login_info_elem.text.split("\n")[-1]
+            self.UTILS.test.TEST(login_info == self.fxa_user, "Login info matches [FxA]")
---
+++ tests/LOOP/test_35817.py
@@ -0,0 +1,47 @@
+#OWD-35817: Login in Loop with MSISDN - SIM detected by app
+
+import time
+from gaiatest import GaiaTestCase
+from OWDTestToolkit.utils.utils import UTILS
+from OWDTestToolkit.apps.loop import Loop
+from OWDTestToolkit.apps.settings import Settings
+from OWDTestToolkit import DOM
+
+
+class main(GaiaTestCase):
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+        self.UTILS = UTILS(self)
+        self.loop = Loop(self)
+        self.settings = Settings(self)
+
+        self.connect_to_network()
+
+        # Clean start
+        if not self.loop.is_installed():
+            self.loop.install()
+            
+        self.loop.launch()
+        try:
+            self.loop.open_settings()
+            self.loop.logout()
+        except:
+            self.UTILS.reporting.logResult('info', "Already logged out")
+
+        self.apps.kill_all()
+        time.sleep(2)
+
+    def tearDown(self):
+        self.UTILS.reporting.reportResults()
+        GaiaTestCase.tearDown(self)
+
+    def test_run(self):
+        # First, login
+        self.loop.launch()
+        result = self.loop.wizard_or_login()
+
+        if result:
+            self.loop.phone_login()
+            self.loop.allow_permission_phone_login()
+            self.UTILS.element.waitForElements(DOM.Loop.app_header, "Loop main view") 
