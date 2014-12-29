5a15680d351834df3544ef8582753831300c894d
Alberto Villena <alberto.villenaguerras@telefonica.com>
2014-12-29 15:43:41 +0100

    Fixed

--- tests/DIALER/test_28539.py
+++ tests/DIALER/test_28539.py
@@ -1,3 +1,17 @@
+# OWD-28539: Press call button after deleting all the calls from the call log 
+# ** Procedure
+#       1. Open call app
+#       2. Open edit mode
+#       3. Press select all button
+#       4. Press delete button
+#       ER1
+#       5. Open dialer app
+#       6. Press call button
+# 
+# ** Expected Results
+#       ER1: All calls are deleted
+#       ER2: display a message within the input area for 3 seconds.
+#
 from gaiatest import GaiaTestCase
 from OWDTestToolkit import DOM
 from OWDTestToolkit.utils.utils import UTILS
@@ -23,6 +37,10 @@ class test_main(GaiaTestCase):
         self.UTILS.reporting.reportResults()
         GaiaTestCase.tearDown(self)
 
+    def _do_the_call(self, number):
+        self.dialer.enterNumber(number, validate=False)
+        self.dialer.call_this_number_and_hangup(5)
+
     def test_run(self):
         self.dialer.launch()
 
@@ -31,73 +49,20 @@ class test_main(GaiaTestCase):
 
         # Call each number
         map(self._do_the_call, self.test_numbers)
-
-        screen_1 = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot of multiple entries:", screen_1)
-
         self.dialer.callLog_clearAll()
 
-        screen_2 = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot of multiple entries removed:", screen_2)
-
-
-    def setUp(self):
-        GaiaTestCase.setUp(self)
-        self.UTILS = UTILS(self)
-        self.contacts = Contacts(self)
-        self.dialer = Dialer(self)
-
-        #Get details of our test contacts.
-        self.Contact_1 = MockContact()
-        self.Contact_2 = MockContact()
-        self.Contact_3 = MockContact()
-        self.Contact_4 = MockContact()
-
-
-        #self.data_layer.insert_contact(self.cont1)
-        #self.data_layer.insert_contact(self.cont2)
-        #self.data_layer.insert_contact(self.cont3)
-
-        self.contact_number_1 = self.Contact_1["tel"]["value"]
-        self.contact_number_2 = self.Contact_2["tel"]["value"]
-        #self.contact_number_3 = self.cont3["tel"]["value"]
-        self.contact_number_twilio = self.Contact_4["tel"]["value"]
-
-    def tearDown(self):
-        self.UTILS.reporting.reportResults()
-        GaiaTestCase.tearDown(self)
-
-    def test_run(self):
-        self.dialer.launch()
-
-        #x = self.UTILS.general.get_config_variable("phone_number", "custom")
-        # create logs entries from contacts
-        self.dialer.createMultipleCallLogEntries(self.contact_number_1, 3)
-        self.dialer.createMultipleCallLogEntries(self.contact_number_2, 2)
-        #self.dialer.createMultipleCallLogEntries(self.contact_number_3, 2)
-        self.dialer.createMultipleCallLogEntries(self.contact_number_twilio, 4)
-
-        x = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot of multiple entries:", x)
-
-        self.dialer.callLog_clearAll()
-
-        x = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screenshot of multiple entries removed:", x)
-
-        #Go back to dialer keypad
+        # Go back to dialer keypad
         keypad_options = self.UTILS.element.getElement(DOM.Dialer.option_bar_keypad, "Keypad Option")
         keypad_options.tap()
 
-        #Tap call button
+        # Tap call button
         call_button = self.UTILS.element.getElement(DOM.Dialer.call_number_button, "Call button")
         call_button.tap()
 
-        #Assert that nothing is presented in the input area
-        x = self.UTILS.element.getElement(DOM.Dialer.phone_number, "Phone number field")
-        dialer_num = x.get_attribute("value")
+        # Assert that nothing is presented in the input area
+        phone_field = self.UTILS.element.getElement(DOM.Dialer.phone_number, "Phone number field")
+        dialer_num = phone_field.get_attribute("value")
         self.assertEqual(dialer_num, "", "Nothing in the input area")
 
-        y = self.UTILS.debug.screenShotOnErr()
-        self.UTILS.reporting.logResult("info", "Screen shot of the result of tapping call button", y)
-
+        screenshot = self.UTILS.debug.screenShotOnErr()
+        self.UTILS.reporting.logResult("info", "Screen shot of the result of tapping call button", screenshot)      
