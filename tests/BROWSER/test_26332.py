380cea874a59736a8a17934346cba0f89eacb8ee
Alberto Villena <alberto.villenaguerras@telefonica.com>
2015-01-12 16:29:14 +0100

    Geolation permissions

--- tests/BROWSER/test_26332.py
+++ tests/BROWSER/test_26332.py
@@ -17,6 +17,7 @@ class test_main(GaiaTestCase):
         self.wifi_pass = self.UTILS.general.get_config_variable("password", "wifi")
 
         self.testURL = self.UTILS.general.get_config_variable("test_url", "common")
+        self.apps.set_permission_by_url(Browser.search_manifest_url, 'geolocation', 'deny')
 
         # switch off keyboard FTU screen
         self.data_layer.set_setting("keyboard.ftu.enabled", False)
@@ -36,6 +37,3 @@ class test_main(GaiaTestCase):
 
         # Open the browser app.
         self.browser.launch()
-
-        # Open our URL.
-        self.browser.open_url(self.testURL)
