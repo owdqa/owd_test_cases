6406c20cbea2b33cd0c893855040a80a49349917
Alberto Villena <alberto.villenaguerras@telefonica.com>
2015-01-21 09:56:53 +0100

    Reset

--- tests/SMS/test_26983.py
+++ tests/SMS/test_26983.py
@@ -32,6 +32,9 @@ class test_main(GaiaTestCase):
         super(test_main, self).__init__(*args, **kwargs)
 
     def setUp(self):
+        #
+        # Set up child objects...
+        #
         GaiaTestCase.setUp(self)
         self.UTILS = UTILS(self)
         self.messages = Messages(self)
@@ -50,7 +53,9 @@ class test_main(GaiaTestCase):
         GaiaTestCase.tearDown(self)
 
     def test_run(self):
+        #
         # Create and send a new test message.
+        #
         test_msg = "email address {} test at {}".format(self.dest_email, time.time())
         self.data_layer.send_sms(self.phone_number, test_msg)
         self.UTILS.statusbar.wait_for_notification_toaster_detail(test_msg, timeout=120)
@@ -58,29 +63,27 @@ class test_main(GaiaTestCase):
         sms = self.messages.last_message_in_this_thread()
         time.sleep(1)
 
+        #
         # Tap the 2nd email link.
+        #
         self.UTILS.reporting.logResult("info", u"Click the email address in this message: '{}'.".format(sms.text))
         _link = sms.find_element("tag name", "a")
         _link.tap()
 
-        send_email_btn = self.UTILS.element.getElement(DOM.Messages.header_send_email_btn, "Send email button")
-        send_email_btn.tap()
+        x = self.UTILS.element.getElement(DOM.Messages.header_send_email_btn, "Send email button")
+        x.tap()
+
+        time.sleep(4)
+
         self.UTILS.iframe.switchToFrame(*DOM.Email.frame_locator)
-        confirm_btn = self.UTILS.element.getElement(DOM.Email.email_not_setup_ok, "Set up account confirmation")
-        confirm_btn.tap()
+        x = self.UTILS.element.getElement(DOM.Email.email_not_setup_ok, "Set up account confirmation")
+        x.tap()
 
+        #
         # Try to set up the account - Since there is no connection, it will fail.
-        self.email.setup_account_first_step(self.email_user, self.email_address)
+        #
+        self.email.setupAccountFirstStep(self.email_user, self.email_address, self.email_pass)
 
-        # Switch to oauth frame and look for error msg
-        # auth_frame = self.marionette.find_element(*('css selector', 'iframe.sup-oauth2-browser'))
-        # self.UTILS.reporting.logResult('info', 'Auth frame: {}'.format(auth_frame.text))
-        self.UTILS.iframe.switchToFrame(*('src', 'oauth2'))
-        element = auth_frame.find_element(*('id', 'error-title'))
-
-        self.UTILS.reporting.logResult('info', "Gimmme da content, you son of a bitch : {}".format(element.text))
-        # self.marionette.switch_to_frame(auth_frame)
-
-        # error = self.UTILS.element.getElement(('id', 'error-title'), "Error message")
-        # self.UTILS.test.test(error.text == "Unable to connect",
-        #     "Verifying error message")
+        error = self.UTILS.element.getElement(DOM.Email.new_account_error_msg, "Error message")
+        self.UTILS.test.test(error.text == "This device is currently offline. Connect to a network and try again.",
+            "Verifying error message")
