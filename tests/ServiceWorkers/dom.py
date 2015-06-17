e9ed91634ee558ac1944bf4a98b7e5b33bb9acdb
Francisco J. Cano Sandoval <fjcs@tid.es>
2015-06-17 09:21:26 +0200

    First basic service worker test

---
+++ tests/ServiceWorkers/dom.py
@@ -0,0 +1,7 @@
+register_btn_dom = ('id', 'register-worker')
+update_worker = ('id', 'update-worker')
+post_message = ('id', 'post-message')
+register_skip_waiting_worker = ('id', 'register-skip-waiting-worker')
+check_controller = ('id', 'check-controller')
+check_registration = ('id', 'check-registration')
+unregister = ('id', 'unregister-registration')
